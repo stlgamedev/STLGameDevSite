449fe63da373b5b430616b936c8a069a8eebcc92
Tim I Hely <seifertim@gmail.com>
2025-05-10 13:14:43 -0500

    trying again

--- scripts/convert-ics-to-json.js
+++ scripts/convert-ics-to-json.js
@@ -8,19 +8,28 @@ const { DateTime } = require('luxon');
 
   for (const k in data) {
     const ev = data[k];
-    if (ev.type === 'VEVENT') {
-      // Assume the intended timezone is America/Chicago
-      const dt = DateTime.fromJSDate(ev.start, { zone: 'utc' }).setZone('America/Chicago');
 
+    
       events.push({
-        title: ev.summary,
-        description: ev.description || '',
-        location: ev.location || '',
-        // This is now correctly localized to Central Time
-        dateTime: dt.toISO(), // e.g., 2025-05-23T17:00:00.000-05:00
-        eventUrl: ev.url || ''
+        ...ev
       });
-    }
+    
+
+
+
+    // if (ev.type === 'VEVENT') {
+    //   // Assume the intended timezone is America/Chicago
+    //   const dt = DateTime.fromJSDate(ev.start, { zone: 'utc' }).setZone('America/Chicago');
+
+    //   events.push({
+    //     title: ev.summary,
+    //     description: ev.description || '',
+    //     location: ev.location || '',
+    //     // This is now correctly localized to Central Time
+    //     dateTime: dt.toISO(), // e.g., 2025-05-23T17:00:00.000-05:00
+    //     eventUrl: ev.url || ''
+    //   });
+    // }
   }
 
   fs.writeFileSync('data/events.json', JSON.stringify(events, null, 2));
